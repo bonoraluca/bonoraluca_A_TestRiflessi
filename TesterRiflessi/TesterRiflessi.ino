71ca13b4f91f3cac4108c2b0f356cc13ab1a9e63
Luca <luca.bonora@barsanti.edu.it>
2019-03-16 09:49:39 +0100

    codiceDaOttimizzare

--- TesterRiflessi/TesterRiflessi.ino
+++ TesterRiflessi/TesterRiflessi.ino
@@ -1,38 +1,55 @@
+#include <LiquidCrystal.h>
+
 int led = 7;
 int ledRgb = 8;
 int btn = 9;
 int btn_ = 10;
 int btn1 = 11;
+int bzr = 6;
 int value = 0;
+LiquidCrystal lcd(12,13,7,8,9,10);
 
 void setup() {
   randomSeed(millis);
   pinMode(led,OUTPUT);
   pinMode(ledRgb,OUTPUT);
   pinMode(btn,INPUT);
+  pinMode(bzr,OUTPUT);
 
 }
 
 void loop() {
-  controllaBottone(int btn, int value);
+  controllaBottone(btn,value);
 
 }
 
 void controllaBottone(int bottone, int value){
   if (value == HIGH){
-    iniziaTest();
+    iniziaTest(0);
   }
 }
 
 void iniziaTest(int tempo){
+  lcd.clear();
   tempo = random(1,5000);
   delay(tempo);
-  while(btn == LOW){
   digitalWrite(led,HIGH);
-  }
-  int ris = cntatempo(led,0);
+  int ris = contatempo(btn_,0);
+  digitalWrite(led,LOW);
   lcd.print(ris + "ms");
-  
+  delay(3000);
+  lcd.clear();
+  tempo = random(3000,5000);
+  delay(tempo);
+  digitalWrite(bzr,HIGH);
+  int ris_ = contatempo(btn1,0);
+  lcd.print(ris_ + "ms");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(ris + "ms");
+  lcd.setCursor(0,1);
+  lcd.print(ris_ + "ms");
 }
 
 
